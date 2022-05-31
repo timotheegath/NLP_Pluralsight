d8de0ad94686ac8778482ae7670c0c147e71c7ad
Timothee Gathmann <timogath@gmail.com>
2022-05-31 21:32:51 +0100

    Up to Disambiguation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # NLP_Pluralsight
+
+## Needed NLTK Packages
+Run the following NLTK commands in Python to download the needed packages for main.py: 
+*nltk.download('punkt')
+*nltk.download('stopwords')
+*nltk.download('averaged_perceptron_tagger')
+*nltk.download('wordnet')
+*nltk.download('omw-1.4')
--- main.py
+++ main.py
@@ -1,2 +1,42 @@
 import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+from nltk.collocations import *
+from string import punctuation
+from nltk.stem.lancaster import LancasterStemmer
 
+text = "Mary had a little lamb. Her fleece was white as snow"
+# Split into sentences
+sents = sent_tokenize(text)
+print(sents)
+# Split into words
+words = [word_tokenize(sent) for sent in sents]
+print(words)
+
+
+#Removing stopwords
+# Custom set of stopwords
+customStopWords = set(stopwords.words('english')+list(punctuation))
+# Remove them from our sentence
+# Split into words
+wordsWithoutStop = [word for word in word_tokenize(text) if word not in customStopWords]
+print(wordsWithoutStop)
+
+# Find N-Grams
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+finder = BigramCollocationFinder.from_words(wordsWithoutStop)
+print(sorted(finder.ngram_fd.items()))
+
+# New sentence to demo stemming
+text2 = "Mary closed on closing night when she was in the mood to close."
+# Lancaster stem
+st = LancasterStemmer()
+stemmedWords = [st.stem(word) for word in word_tokenize(text2)]
+print(stemmedWords) # Word is reduced in stemmed formed
+# POS tagging
+print(nltk.pos_tag(word_tokenize(text2)))
+
+# Word sense disambiguation
+for ss in wn.synsets('bass'):
+    print(ss, ss.definition())
